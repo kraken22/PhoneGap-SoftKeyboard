0ac3a245796da3568d5a39dcb95ad0a03d7d38db
Jordan Jantschulev <j.jantschulev@gmail.com>
2017-09-24 11:46:51 +1000

    fix bug

--- src/android/de/phonostar/SoftKeyboard.java
+++ src/android/de/phonostar/SoftKeyboard.java
@@ -4,51 +4,60 @@ import org.json.JSONArray;
 
 import android.content.Context;
 import android.view.inputmethod.InputMethodManager;
+import android.view.View;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CordovaWebView;
 
 public class SoftKeyboard extends CordovaPlugin {
-
+    
     public SoftKeyboard() {
     }
 
     public void showKeyBoard() {
       InputMethodManager mgr = (InputMethodManager) cordova.getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
-      mgr.showSoftInput(webView, InputMethodManager.SHOW_IMPLICIT);
+      mgr.showSoftInput(getAndroidViewByCordovaWebView(), InputMethodManager.SHOW_IMPLICIT);
 
-      ((InputMethodManager) cordova.getActivity().getSystemService(Context.INPUT_METHOD_SERVICE)).showSoftInput(webView, 0);
+      ((InputMethodManager) cordova.getActivity().getSystemService(Context.INPUT_METHOD_SERVICE)).showSoftInput(getAndroidViewByCordovaWebView(), 0);
     }
 
     public void hideKeyBoard() {
       InputMethodManager mgr = (InputMethodManager) cordova.getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
-      mgr.hideSoftInputFromWindow(webView.getWindowToken(), 0);
+      mgr.hideSoftInputFromWindow(getAndroidViewByCordovaWebView().getWindowToken(), 0);
     }
 
     public boolean isKeyBoardShowing() {
-      int heightDiff = webView.getRootView().getHeight() - webView.getHeight();
+      int heightDiff = getAndroidViewByCordovaWebView().getRootView().getHeight() - getAndroidViewByCordovaWebView()).getHeight();
       return (100 < heightDiff); // if more than 100 pixels, its probably a keyboard...
     }
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) {
-    if (action.equals("show")) {
-      this.showKeyBoard();
-      callbackContext.success("done");
-      return true;
+        if (action.equals("show")) {
+          this.showKeyBoard();
+          callbackContext.success("done");
+          return true;
+        }
+        else if (action.equals("hide")) {
+          this.hideKeyBoard();
+          callbackContext.success();
+          return true;
+        }
+        else if (action.equals("isShowing")) {
+          callbackContext.success(Boolean.toString(this.isKeyBoardShowing()));
+          return true;
+        }
+        else {
+          return false;
+        }
     }
-    else if (action.equals("hide")) {
-      this.hideKeyBoard();
-      callbackContext.success();
-      return true;
+    
+    private View getAndroidViewByCordovaWebView(){
+        if (webView instanceof View){
+            return webView;
+        } else {
+            return webView.getView();   
+        }
     }
-    else if (action.equals("isShowing")) {
-      callbackContext.success(Boolean.toString(this.isKeyBoardShowing()));
-      return true;
-    }
-    else {
-      return false;
-    }
-  }
 }
-
